60a17d4e6a8390a683ba2f82dd3b23a3c7f8e600
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-25 12:22:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    puts params
+  # "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
+
 
 end
